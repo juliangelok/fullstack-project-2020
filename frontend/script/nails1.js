fac7530894a7a4d8e2016da12eda000c91fd4802
juliangelok <carnatriya@gmail.com>
2020-01-16 23:31:48 +0300

    initial commit

---
+++ frontend/script/nails1.js
@@ -0,0 +1,15 @@
+function next1(){
+	document.location.href = "light.html";
+}
+function next2(){
+	document.location.href = "bright.html";
+}
+function next3(){
+	document.location.href = "dark.html";
+}
+function newyear(){
+	document.location.href = "newyear.html";
+}
+function hellowin(){
+	document.location.href = "hellowin.html";
+}
